8ba4cf4769865dca527a8e72c137708d14ce47e8
toptopgo <toptopgoinfo@gmail.com>
2026-07-19 20:53:58 +0100

    fix: trust Railway proxy pour generer les bonnes URLs https (assets)

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,9 +10,14 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array
+     * Railway (comme Heroku) fait transiter les requêtes derrière un proxy
+     * dont l'IP n'est pas fixe. Sans ça, Laravel ignore X-Forwarded-Proto
+     * et génère des URLs en http:// même quand le visiteur est en https://,
+     * ce qui casse le chargement des assets (CSS/JS) en contenu mixte.
+     *
+     * @var array|string
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
